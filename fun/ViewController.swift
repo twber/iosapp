b9411d7d4696c7f75c2ccff9e672c26be6a693a6
twber <hctsai@umich.edu>
2020-07-15 16:08:41 +0800

    Initial Commit

---
+++ fun/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fun
+//
+//  Created by Bernice TSAI on 2020/7/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
